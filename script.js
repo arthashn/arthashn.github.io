89ab10f2b120e97da8d5365f46ce1868f40d172b
arthashn <arthashn200602@gmail.com>
2025-03-18 13:16:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let line = document.getElementById("animatedLine");
+    let growing = true;
+
+    setInterval(() => {
+        if (growing) {
+            line.style.width = "150px";
+            line.style.backgroundColor = "red";
+        } else {
+            line.style.width = "50px";
+            line.style.backgroundColor = "blue";
+        }
+        growing = !growing;
+    }, 1000);
+});
+
 document.getElementById("ugmButton").addEventListener("click", function() {
-    window.location.href = "https://lib.ugm.ac.id/";
+    window.open("https://lib.ugm.ac.id/", "_blank");
 });

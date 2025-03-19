9e94bd2b3f3ec8d80b9a2c1554ca244d567e180d
arthashn <arthashn200602@gmail.com>
2025-03-19 18:39:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -18,23 +18,10 @@ document.getElementById("ugmButton").addEventListener("click", function() {
     window.open("https://lib.ugm.ac.id/", "_blank");
 });
 
-    function updateDateTime() {
-        const now = new Date();
-        const options = {
-            weekday: 'long',
-            day: 'numeric',
-            month: 'long',
-            year: 'numeric',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit'
-        };
-        let dateTimeElement = document.getElementById("datetime");
-        if (dateTimeElement) {
-            dateTimeElement.textContent = now.toLocaleDateString('id-ID', options);
-        }
-    }
+function updateDateTime() {
+    let now = new Date();
+    document.getElementById("datetime").innerHTML = now.toLocaleString();
+}
 
-    setInterval(updateDateTime, 1000);
-    updateDateTime();
+setInterval(updateDateTime, 1000);
 });
